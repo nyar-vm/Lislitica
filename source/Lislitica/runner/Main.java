41322390a00f6218219f52faeac5a26852577346
Aster <galaster@foxmail.com>
2019-05-20 14:17:53 +0800

    💉 Test basic data type

--- source/Lislitica/runner/Main.java
+++ source/Lislitica/runner/Main.java
@@ -1,23 +1,27 @@
-package nyar;
+package Lislitica.runner;
 
 import java.io.File;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
+import Lislitica.antlr.LaLexer;
+import Lislitica.antlr.LaParser;
+import Lislitica.antlr.LaVisitor;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        String Test = "condition"; //"condition";
-        CharStream input = CharStreams.fromFileName("test/" + Test + ".nyar");
+        String Test = "data"; //"condition";
+        CharStream input = CharStreams.fromFileName("test/" + Test + ".la");
         File output = new File("test/" + Test + ".m");
 
-        NyarLexer lexer = new NyarLexer(input);
+        LaLexer lexer = new LaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        NyarParser parser = new NyarParser(tokens);
-        NyarVisitor translator = new Translator();
+        LaParser parser = new LaParser(tokens);
+        LaVisitor translator = new Translator();
         String MExpression = String.format("%s", translator.visit(parser.program()));
         System.out.print("========================================\n" + MExpression);
 
