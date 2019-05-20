7a077b030140bdfd758aafef0fd5980109f35f04
Aster <galaster@foxmail.com>
2019-05-20 16:20:12 +0800

    🚀 Formatter of list and dict

--- source/Lislitica/runner/Translator.java
+++ source/Lislitica/runner/Translator.java
@@ -3,6 +3,9 @@ package Lislitica.runner;
 import Lislitica.antlr.LaBaseVisitor;
 import Lislitica.antlr.LaParser;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Translator extends LaBaseVisitor<String> {
     public String visitProgram(LaParser.ProgramContext ctx) {
         StringBuilder result = new StringBuilder();
@@ -27,20 +30,51 @@ public class Translator extends LaBaseVisitor<String> {
         //System.out.print("Literal: " + ctx.getText() + "\n");
         return ctx.getText();
     }
+
+    // List
     public String visitLiteralList(LaParser.LiteralListContext ctx) {
-        System.out.print("Data: " + ctx.getText() + "\n");
-        return visitChildren(ctx);
+        // System.out.print("Data: " + ctx.getText() + "\n");
+        Stream<String> terms = ctx.element().stream().map(this::visit);
+        int length = ctx.getText().length();
+        if (length > 120) {
+            String result = terms.collect(Collectors.joining(",\n"));
+            return "{\n" + result.replaceAll("(?m)^", "\t") + "\n}";
+        } else {
+            return "{" + terms.collect(Collectors.joining(", ")) + "}";
+        }
     }
+
     public String visitEmptyList(LaParser.EmptyListContext ctx) {
-        System.out.print("Data: " + ctx.getText() + "\n");
+        //System.out.print("Data: " + ctx.getText() + "\n");
         return "[ ]";
     }
+
+    // Dict
     public String visitLiteralDict(LaParser.LiteralDictContext ctx) {
-        System.out.print("Data: " + ctx.getText() + "\n");
-        return visitChildren(ctx);
+        //System.out.print("Data: " + ctx.getText() + "\n");
+        Stream<String> terms = ctx.keyValue().stream().map(this::visit);
+        int length = ctx.getText().length();
+        if (length > 120) {
+            String result = terms.map(i -> "\t" + i + ",").collect(Collectors.joining("\n"));
+            return "<|\n" + result.substring(0, result.length() - 1) + "\n|>";
+        } else {
+            return "<|" + terms.collect(Collectors.joining(", ")) + "|>";
+        }
     }
+
     public String visitEmptyDict(LaParser.EmptyDictContext ctx) {
-        System.out.print("Data: " + ctx.getText() + "\n");
+        //System.out.print("Data: " + ctx.getText() + "\n");
         return "<| |>";
+    }
+
+    public String visitSymbolKey(LaParser.SymbolKeyContext ctx) {
+        String k = ctx.key.getText();
+        String v = this.visit(ctx.value);
+        //System.out.print("Key: " + ctx.getText() + "\n");
+        if (ctx.Power() != null) {
+            return String.format("%s -> %s", k, v);
+        } else {
+            return String.format("\"%s\" -> %s", k, v);
+        }
     }
 }
